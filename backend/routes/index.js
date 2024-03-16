1607db38bb1dfb884f2069adc0ce2bd07c733395
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-03-16 15:26:53 +0000

    Node + Express Setup

---
+++ backend/routes/index.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/test", function (req, res, next) {
+  return res.status(200).json({ message: "Success" });
+});
+
+module.exports = router;
