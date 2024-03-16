001bca7bcfe2789a724bca90ed694d9d8179d170
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-03-16 17:41:34 +0000

    Created Cron Job

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,33 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { CronJob } = require("cron");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.post("/test", function (req, res, next) {
-  return res.status(200).json({ message: "Success" });
+router.post("/schedule", function (req, res, next) {
+  const { transactionId, duration } = req.body;
+  var runCount = 0;
+
+  const job = new CronJob(`*/${duration} * * * * *`, () => {
+    if (runCount === 2) job.stop();
+
+    console.log(
+      `${
+        runCount + 1
+      }. Running API for transaction ID: ${transactionId} every ${duration} seconds`
+    );
+
+    runCount++;
+  });
+
+  job.start();
+
+  res.status(200).json({
+    message: `Monitoring scheduled for transaction ID: ${transactionId}`,
+  });
 });
 
 module.exports = router;
