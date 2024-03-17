0cbeaeb45f17f73e8771bd8c6c02a35c6c284692
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-03-17 10:40:52 +0000

    supporting scripts

---
+++ arbs/k.ts
@@ -0,0 +1,98 @@
+const { providers, Wallet, ethers } = require('ethers')
+const {
+  getL2Network,
+  addDefaultLocalNetwork,
+} = require('@arbitrum/sdk/dist/lib/dataEntities/networks')
+const { InboxTools } = require('@arbitrum/sdk')
+const {
+  ArbSys__factory,
+} = require('@arbitrum/sdk/dist/lib/abi/factories/ArbSys__factory')
+
+const {
+  ARB_SYS_ADDRESS,
+} = require('@arbitrum/sdk/dist/lib/dataEntities/constants')
+
+require("dotenv").config()
+
+/**
+ * Set up: instantiate L1 / L2 wallets connected to providers
+ */
+const walletPrivateKey = process.env.DEVNET_PRIVKEY
+console.log(walletPrivateKey)
+
+const l1Provider = new providers.JsonRpcProvider(process.env.L1RPC)
+const l2Provider = new providers.JsonRpcProvider(process.env.L2RPC)
+
+const l1Wallet = new Wallet(walletPrivateKey, l1Provider)
+const l2Wallet = new Wallet(walletPrivateKey, l2Provider)
+
+const main = async () => {
+
+  /**
+   * Add the default local network configuration to the SDK
+   * to allow this script to run on a local node
+   */
+  addDefaultLocalNetwork()
+
+  const l2Network = await getL2Network(l2Provider)
+
+  const inboxSdk = new InboxTools(l1Wallet, l2Network)
+
+  /**
+   * Here we have a arbsys abi to withdraw our funds; we'll be setting it by sending it as a message from delayed inbox!!!
+   */
+
+  console.log("===")
+  const arbSys = ArbSys__factory.connect(ARB_SYS_ADDRESS, l1Wallet.provider)
+  console.log("===")
+
+  const arbsysIface = arbSys.interface
+  const calldatal2 = arbsysIface.encodeFunctionData('withdrawEth', [
+    l1Wallet.address,
+  ])
+
+  const transactionl2Request = {
+    data: calldatal2,
+    to: ARB_SYS_ADDRESS,
+    value: 1, // Only set 1 wei since it just a test tutorial, you can set whatever you want in real runtime.
+  }
+
+  /**
+   * We need extract l2's tx hash first so we can check if this tx executed on l2 later.
+   */
+  const l2SignedTx = await inboxSdk.signL2Tx(transactionl2Request, l2Wallet)
+
+  const l2Txhash = ethers.utils.parseTransaction(l2SignedTx).hash
+
+  const resultsL1 = await inboxSdk.sendL2SignedTx(l2SignedTx)
+
+  const inboxRec = await resultsL1.wait()
+
+  console.log(`Withdraw txn initiated on L1! 🙌 ${inboxRec.transactionHash}`)
+
+  /**
+   * Now we successfully send the tx to l1 delayed inbox, then we need to wait the tx to be executed on l2
+   */
+  console.log(
+    `Now we need to wait tx: ${l2Txhash} to be included on l2 (may take 15 minutes, if longer than 1 day, you can use sdk to force include) ....... `
+  )
+
+  const l2TxReceipt = await l2Provider.waitForTransaction(l2Txhash)
+
+  const status = l2TxReceipt.status
+  if (status == true) {
+    console.log(
+      `L2 txn executed!!! 🥳 , you can go to https://bridge.arbitrum.io/ to execute your withdrawal and recieve your funds after challenge period!`
+    )
+  } else {
+    console.log(`L2 txn failed, see if your gas is enough?`)
+    return
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
---
+++ arbs/z.ts
@@ -0,0 +1,118 @@
+import { providers, Wallet, utils } from 'ethers';
+import { ContractTransaction, Signer } from 'ethers'
+import { network } from 'hardhat'
+import * as dotenv from "dotenv";
+import { Bridge__factory } from '@arbitrum/sdk/dist/lib/abi/factories/Bridge__factory';
+import { L2Network, getL1Network, getL2Network } from '@arbitrum/sdk/dist/lib/dataEntities/networks'
+import { BigNumber } from '@ethersproject/bignumber'
+import { Inbox__factory } from '@arbitrum/sdk/dist/lib/abi/factories/Inbox__factory'
+import { addDefaultLocalNetwork } from '@arbitrum/sdk/dist/lib/dataEntities/networks'
+
+import { InboxTools } from '@arbitrum/sdk';
+import {SequencerInbox__factory} from "@arbitrum/sdk/dist/lib/abi/factories/SequencerInbox__factory"
+
+dotenv.config()
+
+/**
+ * Set up: instantiate L1 / L2 wallets connected to providers
+ */
+const walletPrivateKey: string = process.env.DEVNET_PRIVKEY!
+
+const l1Provider = new providers.JsonRpcProvider(process.env.L1RPC)
+const l2Provider = new providers.JsonRpcProvider(process.env.L2RPC)
+
+
+const l1Wallet = new Wallet(walletPrivateKey, l1Provider)
+const l2Wallet = new Wallet(walletPrivateKey, l2Provider)
+
+const submitL2Tx = async (
+  tx: {
+    to: string
+    value?: BigNumber
+    data?: string
+    nonce: number
+    gasPriceBid: BigNumber
+    gasLimit: BigNumber
+  },
+  l2Network: L2Network,
+  l1Signer: Signer
+): Promise<ContractTransaction> => {
+  const inbox = Inbox__factory.connect(l2Network.ethBridge.inbox, l1Signer)
+
+  return await inbox.sendUnsignedTransaction(
+    tx.gasLimit,
+    tx.gasPriceBid,
+    tx.nonce,
+    tx.to,
+    tx.value || BigNumber.from(21000000000),
+    tx.data || '0x'
+  )
+}
+
+const setup = async () => {
+
+  addDefaultLocalNetwork()
+
+  const signer = l1Wallet
+  const provider = signer.provider
+
+    
+  const arbitrumOne = await getL2Network(l2Provider)
+  const l1network = await getL1Network(l1Provider)
+
+
+  const sequencerInbox = SequencerInbox__factory.connect(
+    arbitrumOne.ethBridge.sequencerInbox,
+    provider
+  )
+
+  const bridge = Bridge__factory.connect(
+    arbitrumOne.ethBridge.bridge,
+    provider
+  )
+
+
+  return {
+    l1Signer: signer,
+    l1Provider: provider,
+    l2Network: arbitrumOne,
+    sequencerInbox,
+    bridge,
+  }
+}
+
+
+
+const main = async () => {
+
+    const { l1Signer, l2Network, sequencerInbox, bridge } = await setup()
+
+    const inboxTools = new InboxTools(l1Signer, l2Network)
+    // const l2Tx = await submitL2Tx(
+    //   {
+    //     to: await l1Signer.getAddress(),
+    //     value: BigNumber.from(1000000),
+    //     gasLimit: BigNumber.from(100000),
+    //     gasPriceBid: BigNumber.from(21000000000),
+    //     nonce: 0,
+    //   },
+    //   l2Network,
+    //   l1Signer
+    // )
+    // await l2Tx.wait()
+
+    const forceInclusionTx = await inboxTools.forceInclude()
+
+    await forceInclusionTx!.wait()
+    // const messagesReadAfter = await sequencerInbox.totalDelayedMessagesRead()
+
+
+  
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
